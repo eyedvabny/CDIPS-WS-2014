0641593bef05fa6eb16e692dc67a27697e6ae979
eyedvabny <eugene@yedvabny.com>
2014-07-30 19:29:43 -0700

    Fix private variables to make an actual module

--- avito_modules.py
+++ avito_modules.py
@@ -5,28 +5,28 @@ Extracted from Avito-provided sample code
 """
 
 #RegExp used in correctWord()
-import re
+import re as _re
 
 # NLTK: natural language processing
-import nltk.corpus
-from nltk import SnowballStemmer
+import nltk.corpus as _nc
+from nltk import SnowballStemmer as _ss
 
-# Use Pandas for parsing large tables
-import pandas as pd
+#Pandas for Series manipulation
+import pandas as _pd
 
 #Filter out useless (stop) words
-stopwords= frozenset(word.decode('utf-8') for word in nltk.corpus.stopwords.words("russian") if word!="не")    
+_stopwords= frozenset(word.decode('utf-8') for word in _nc.stopwords.words("russian") if word!="не")    
 
 #Reduce the words down to their stems
 #http://snowball.tartarus.org/algorithms/russian/stemmer.html
-stemmer = SnowballStemmer('russian')
+_stemmer = _ss('russian')
 
 #Some cyrillic letters look like latin letters but have different unicode
 #Since unicode have to use ord() to get Unicode ordinals
-engChars = [ord(char) for char in u"cCyoOBaAKpPeE"]
-rusChars = [ord(char) for char in u"сСуоОВаАКрРеЕ"]
-eng_rusTranslateTable = dict(zip(engChars, rusChars))
-rus_engTranslateTable = dict(zip(rusChars, engChars))
+_engChars = [ord(_char) for _char in u"cCyoOBaAKpPeE"]
+_rusChars = [ord(_char) for _char in u"сСуоОВаАКрРеЕ"]
+_eng_rusTranslateTable = dict(zip(_engChars, _rusChars))
+_rus_engTranslateTable = dict(zip(_rusChars, _engChars))
 
 def correctWord (w):
     """ Corrects word by replacing characters with written similarly depending on which language the word. 
@@ -40,10 +40,10 @@ def correctWord (w):
     # Assume russian is the main language and translate the rogue english letter
     # Otherwise translate the rogue russian letters in english words
     # Words from getWords will be all lowercase
-    if len(re.findall(ur"[а-я]",w))>len(re.findall(ur"[a-z]",w)):
-        return w.translate(eng_rusTranslateTable)
+    if len(_re.findall(ur"[а-я]",w))>len(_re.findall(ur"[a-z]",w)):
+        return w.translate(_eng_rusTranslateTable)
     else:
-        return w.translate(rus_engTranslateTable)
+        return w.translate(_rus_engTranslateTable)
 
 def getWords(text, stemmRequired = False, correctWordRequired = False):
     """ Splits the text into words, discards stop words and applies stemmer. 
@@ -59,13 +59,13 @@ def getWords(text, stemmRequired = False, correctWordRequired = False):
         return u''
 
     # Change to lowercase and remove all non-alphanumeric characters
-    cleanText = re.sub(u'[^a-zа-я]', ' ', text.lower())
+    cleanText = _re.sub(u'[^a-zа-я]', ' ', text.lower())
     
     # Check if we want to fix up mixed english-russian words
     if correctWordRequired:
-        words = [correctWord(w) if not stemmRequired else stemmer.stem(correctWord(w)) for w in cleanText.split() if len(w)>1 and w not in stopwords]
+        words = [correctWord(w) if not stemmRequired else _stemmer.stem(correctWord(w)) for w in cleanText.split() if len(w)>1 and w not in _stopwords]
     else:
-        words = [w if not stemmRequired else stemmer.stem(w) for w in cleanText.split() if len(w)>1 and w not in stopwords]
+        words = [w if not stemmRequired else _stemmer.stem(w) for w in cleanText.split() if len(w)>1 and w not in _stopwords]
     
     return words
 
@@ -77,15 +77,8 @@ def splitIntoWords(textSeries, stemmRequired = False, correctWordRequired = Fals
     stemmRequired : bool - flag whether stemming required
     correctWordRequired : bool - flag whether correction of words required
     """
-    # Convert the array into a list
-    string_list = textSeries.tolist()
-
-    # Run the function on every string in the list
-    word_list = [word for ugly_string in string_list for word in getWords(ugly_string,stemmRequired,correctWordRequired)]
+    # Run the getWords function on every string in the list
+    word_list = [word for ugly_string in textSeries for word in getWords(ugly_string,stemmRequired,correctWordRequired)]
     
     # Filter out
-    return pd.Series(word_list).drop_duplicates()
-
-
-
-
+    return _pd.Series(word_list).drop_duplicates()
