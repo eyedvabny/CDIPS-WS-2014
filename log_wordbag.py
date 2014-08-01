5e15989fa7c6796692323f3f7c963db2321ef871
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-08-01 12:48:49 -0700

    Rewrote log_wordbag to run as two callable scripts; validate runs test on training data; run trains on train and tests on test

--- log_wordbag.py
+++ log_wordbag.py
@@ -19,46 +19,70 @@ from sklearn.metrics import average_precision_score
 from sklearn.cross_validation import train_test_split
 import os
 
-data = pd.read_table('/media/dillon/dinsfire/avito_train.tsv',nrows=1000)
-test = pd.read_table('/media/dillon/dinsfire/avito_test.tsv',nrows=1000)
-#replace with file path to your training data
+def validate(input_train, rows=True, test=0.25)
+    """
+    Takes file as input and returns classification report, average precision, and
+    AUC for a bigram model. By default, loads all rows of a dataset, trains on .75,
+    and tests on .25. 
+    ----
+    input_train : 'full path of the file you are loading'
+    rows : True - loads all rows; insert an int for specific number of rows
+    test : float proportion of dataset used for testing
+    """
+    if rows = True:
+        data = pd.read_table(input_train)
+    else:
+        data = pd.read_table(input_train, nrows = rows)
+    response = data.is_blocked
+    dummies = sparse.csc_matrix(pd.get_dummies(data.subcategory))
+    words = np.array(data.description,str)
+    del data
+    vect = text.CountVectorizer(decode_error = u'ignore',strip_accents='unicode',ngram_range=(1,2))
+    counts = vect.fit_transform(words)
+    features = sparse.hstack((dummies,counts))
+    features_train, features_test, target_train, target_test = train_test_split(features, response, test_size = test)
+    clf = linear_model.LogisticRegression()
+    clf.fit(features_train, target_train)
+    prediction = clf.predict(features_test)
+    return classification_report(target_test, prediction), average_precision_score(target_test, prediction), roc_auc_score(target_test, prediction)
 
-testItemIds = test.itemid
-response = data.is_blocked
-dummies = sparse.csc_matrix(pd.get_dummies(data.subcategory))
-pretestdummies = pd.get_dummies(test.subcategory)
-testdummies = sparse.csc_matrix(pretestdummies.drop(['Растения', 'Товары для компьютера'],axis=1))
-words = np.array(data.description,str)
-testwords = np.array(test.description,str)
-del data, test
-vect = text.CountVectorizer(decode_error = u'ignore',strip_accents='unicode',ngram_range=(1,2))
-corpus = np.concatenate((words, testwords))
-vect.fit(corpus)
-counts = vect.transform(words)
+def run(input_train, input_test, output_name)
+    """
+    Takes a file path as input, a file path as output, and produces a sorted csv of
+    item IDs for Kaggle submission
+    -------
+    input_train : 'full path of the training file'
+    input_test : 'full path of the testing file'
+    output_name : 'full path of the output file'
+    """
 
-features = sparse.hstack((dummies,counts))
-features_train, features_test, target_train, target_test = train_test_split(features, response, test_size = 0.25)
-
-clf = linear_model.LogisticRegression()
-clf.fit(features_train, target_train)
-prediction = clf.predict(features_test)
-print classification_report(target_test, prediction)
-print average_precision_score(target_test, prediction)
-print roc_auc_score(target_test, prediction)
-
-#stop here if you do not want to create a kaggle file
-
-clf.fit(features, response)
-testcounts = vect.transform(testwords)
-testFeatures = sparse.hstack((testdummies,testcounts))
-predicted_scores = clf.predict_proba(testFeatures).T[1]
-dataFolder = "~/Dropbox/pydir/CDIPS-WS-2014"
-output_file = "log_wordbag_solution.csv"
-f = open('/home/dillon/Dropbox/pydir/CDIPS-WS-2014/log_wordbag_solution.csv','w')
-f.write("id\n") 
-for pred_score, item_id in sorted(zip(predicted_scores, testItemIds), reverse = True):
-    f.write("%d\n" % (item_id))
-f.close()
+    data = pd.read_table(input_train)
+    test = pd.read_table(input_test)
+    testItemIds = test.itemid
+    response = data.is_blocked
+    dummies = sparse.csc_matrix(pd.get_dummies(data.subcategory))
+    pretestdummies = pd.get_dummies(test.subcategory)
+    testdummies = sparse.csc_matrix(pretestdummies.drop(['Растения', 'Товары для компьютера'],axis=1))
+    words = np.array(data.description,str)
+    testwords = np.array(test.description,str)
+    del data, test
+    vect = text.CountVectorizer(decode_error = u'ignore', strip_accents='unicode', ngram_range=(1,2))
+    corpus = np.concatenate((words, testwords))
+    vect.fit(corpus)
+    counts = vect.transform(words)
+    features = sparse.hstack((dummies,counts))
+    clf = linear_model.LogisticRegression()
+    clf.fit(features, response)
+    testcounts = vect.transform(testwords)
+    testFeatures = sparse.hstack((testdummies,testcounts))
+    predicted_scores = clf.predict_proba(testFeatures).T[1]
+    dataFolder = "~/Dropbox/pydir/CDIPS-WS-2014"
+    output_file = "log_wordbag_solution.csv"
+    f = open('/home/dillon/Dropbox/pydir/CDIPS-WS-2014/log_wordbag_solution.csv','w')
+    f.write("id\n") 
+    for pred_score, item_id in sorted(zip(predicted_scores, testItemIds), reverse = True):
+        f.write("%d\n" % (item_id))
+    f.close()
 
 
 
